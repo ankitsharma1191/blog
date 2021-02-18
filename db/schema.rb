41573d744e60415cc03489525dcb78d92ad3f6b3
Ankit Sharma <ankitsharma.1191@gmail.com>
2021-02-19 01:28:32 +0530

    chef and Recipe associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_190554) do
+ActiveRecord::Schema.define(version: 2021_02_18_192701) do
 
   create_table "articles", charset: "utf8mb4", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2021_02_18_190554) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "chef_id"
   end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
