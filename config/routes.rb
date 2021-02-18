abd18ac9d2d9aba7216a45d6c2dd3cafd7f6ff76
Ankit Sharma <ankitsharma.1191@gmail.com>
2021-02-19 02:13:53 +0530

    listing recipes are done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+	resources :recipes
 end
